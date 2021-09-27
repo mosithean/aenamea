fc9061c7e862ff848d857703f2e1b5e11c71161e
Valentin Lepmets <mosithean@gmail.com>
2021-09-27 16:35:51 +0200

    Use JDBC Template for executing SQL queries
    config:
    - use JdbcTemplate as JDBC Wrapper
    
    movie:
    - do jdbcTemplate.update for insertMovie(method)

--- src/main/java/com/msthn/movie/MovieDataAccessService.java
+++ src/main/java/com/msthn/movie/MovieDataAccessService.java
@@ -9,6 +9,12 @@ import java.util.Optional;
 @Repository
 public class MovieDataAccessService implements MovieDao {
 
+    private final JdbcTemplate jdbcTemplate;
+
+    public MovieDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public List<Movie> selectMovies() {
         throw new UnsupportedOperationException("not implemented");
@@ -16,7 +22,14 @@ public class MovieDataAccessService implements MovieDao {
 
     @Override
     public int insertMovie(Movie movie) {
-        throw new UnsupportedOperationException("not implemented");
+        String sql = """
+                 INSERT INTO movie(name, release_date) 
+                 VALUES (?, ?);
+                """;
+        return jdbcTemplate.update(
+                sql,
+                movie.name(), movie.releaseDate()
+        );
     }
 
     @Override
@@ -29,5 +42,4 @@ public class MovieDataAccessService implements MovieDao {
     public Optional<Movie> selectMovieById(int id) {
         throw new UnsupportedOperationException("not implemented");
     }
-    
 }
